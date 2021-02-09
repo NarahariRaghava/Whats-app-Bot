9ab38aa97134293da7c90d7fedc232bc9bfff40f
Narahari Raghava <70995755+narahari-7@users.noreply.github.com>
2021-02-09 09:47:22 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,4 +1,4 @@
 import pywhatkit 
-pywhatkit.sendwhatmsg(+919848425261,'Hello Good Morning',9,34)
+pywhatkit.sendwhatmsg('+919848425261','Hello Good Morning',9,34)
 #enter the phone number using country code. 
 #Time in 24hr format.
